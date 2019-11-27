283f272cfe2cd94e79584b09e775bc9a7f2700bf
Franziska von Bethmann <franziska.bethmann@gmail.com>
2019-11-27 17:59:05 +0100

    change search and homepage design

--- app/controllers/packages_controller.rb
+++ app/controllers/packages_controller.rb
@@ -2,7 +2,14 @@ class PackagesController < ApplicationController
   before_action :set_package, only: :show
   skip_before_action :authenticate_user!, only: [:index, :show]
 
-  def index; end
+  def index
+    if params[:query].present?
+      @packages = Package.where(dep_city: params[:dep_city], arr_city: params[:arr_city], start_date: params[:start_date], end_date: params[:end_date])
+    else
+      @packages = Package.all
+    end
+    raise
+  end
 
   def show
     # @booking = Booking.new
