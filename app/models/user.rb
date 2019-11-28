70bf45eeb737ac0a33f47d83a14dba02da29d325
moritz gosmann <moritz.gosmann@outlook.de>
2019-11-28 12:22:15 +0100

    latest changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,14 +6,13 @@ class User < ApplicationRecord
   belongs_to :company
   has_many :bookings
 
- # before_validation :assign_company
+  before_validation :assign_company
 
-  #validates :first_name, presence: true
-  #validates :last_name, presence: true
-  #validates :email, presence: true, uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true
 
- # def assign_company
-  #  self.company = Company.first
-    # raise
- # end
+  def assign_company
+    self.company = Company.first
+  end
 end
