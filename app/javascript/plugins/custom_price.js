d333551bb3432db65b7df699189ca52a4baaf5cd
Marina Guseva <marina.guseva@contentful.com>
2019-12-02 19:14:06 +0100

    Add slider

---
+++ app/javascript/plugins/custom_price.js
@@ -0,0 +1,19 @@
+const showSliderValue = () => {
+  const rangeBullet = document.getElementById("rs-bullet");
+  let rangeSlider = document.getElementById("company_policy_max_price_train");
+
+  rangeBullet.innerHTML = rangeSlider.value;
+  let bulletPosition = (rangeSlider.value / rangeSlider.max);
+  console.log(bulletPosition * 1300)
+  // TODO fix the position - should be 39
+  rangeBullet.style.left = (bulletPosition * 578) + "px";
+
+}
+
+const priceRange = () => {
+  const rangeSlider = document.getElementById("company_policy_max_price_train");
+  rangeSlider.addEventListener("input", showSliderValue, false);
+  showSliderValue();
+}
+
+export { priceRange };
