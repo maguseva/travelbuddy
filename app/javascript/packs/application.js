03d63a1045aea6663c8c073c250af8486e1f7b32
Julian Bodenschatz <j.a.b@posteo.de>
2019-12-02 16:12:46 +0100

    add tabs

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,4 +3,16 @@ import "../plugins/flatpickr";
 
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 import { initMapbox } from '../plugins/init_mapbox';
+
 initMapbox();
+
+// TABS
+const tabs = document.querySelectorAll('.tab');
+const tabsContent = document.querySelectorAll('.schedule');
+
+tabs.forEach(tab => tab.addEventListener("click", (event) => {
+  if (!tab.classList.contains('active')) {
+    tabs.forEach( t => t.classList.toggle('active'));
+    tabsContent.forEach( tc => tc.classList.toggle('hidden'));
+  }
+}));
