5fe0bded95ebc4458ba6cf8e329512ecb6b2b56c
Franziska von Bethmann <franziska.bethmann@gmail.com>
2019-12-03 18:14:59 +0100

    fixed buttons bookings, implemented ticket-modal

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :user
-  belongs_to :package, dependent: :destroy
+  belongs_to :package
   validates :status, inclusion: { in: ["pending", "paid", "cancelled"] }
   validates :user, presence: true
   validates :package, presence: true
