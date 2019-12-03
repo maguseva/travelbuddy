7e89b1b29d0d56c3606435be1ff1161084ee06f2
Franziska von Bethmann <franziska.bethmann@gmail.com>
2019-12-03 10:28:22 +0100

    testing search design

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,10 +3,9 @@ import "../plugins/flatpickr";
 
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 import { initMapbox } from '../plugins/init_mapbox';
+import { initAutocomplete } from '../plugins/init_autocomplete';
 
 initMapbox();
-
-import { initAutocomplete } from '../plugins/init_autocomplete'
 initAutocomplete();
 
 // TABS
